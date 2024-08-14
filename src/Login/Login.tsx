81f7657873440e2e7d6a51ed8a2a41b363a41078
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-08-14 10:58:57 +0530

    fix

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -17,7 +17,7 @@ export const Login = () => {
             const res = await Ajax.sendPostReq("std/login", data)
             console.log(res);
             if (res?.data?.length > 0) {
-                if (typeof sessionStorage !== "undefined") {
+                if (typeof window !== 'undefined') {
                     sessionStorage.user = res?.data?.[0]?.uid
                 }
                 dispatch({ type: "LOGIN", payload: { isLoggedIn: true, user: res?.data?.[0]?.uid } })
